d9eae82e26292615d61acbdf2f10dadee81d4e91
Deep Agarwal <deepagarwal@Deeps-MacBook-Air.local>
2026-07-28 19:06:29 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, UploadFile, File
+import whisper
+import tempfile
+import os
+
+app = FastAPI()
+
+print("Loading Whisper model...")
+model = whisper.load_model("base")
+print("Whisper model loaded!")
+
+@app.post("/transcribe")
+async def transcribe(audio: UploadFile = File(...)):
+    suffix = os.path.splitext(audio.filename)[1]
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as temp_audio:
+        temp_audio.write(await audio.read())
+        temp_path = temp_audio.name
+
+    result = model.transcribe(
+    temp_path,
+    language="hi"
+)
+
+    os.remove(temp_path)
+
+    return {
+    "text": result["text"],
+    "language": result["language"]
+}
